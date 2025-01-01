77c5eced57f83da164217029ec723ac8bff4063a
mchambers49 <mchambers49@gatech.edu>
2024-12-31 23:47:37 -0800

    Created Binary Search
    Created binary search searching algorithm.

--- DSAPractice/src/main/java/org/example/Algorithms/SearchingAlgorithms.java
+++ DSAPractice/src/main/java/org/example/Algorithms/SearchingAlgorithms.java
@@ -4,7 +4,7 @@ public class SearchingAlgorithms {
 
 
     /**
-     * Linear Search: 
+     * Linear Search:
      * Big O: O(N)
      *
      * @param numbers array of integers
@@ -15,6 +15,40 @@ public class SearchingAlgorithms {
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] == searchValue) {
                 return i;
+            }
+        }
+        return -1; // -1 is returned if the search value doesn't exist in the array
+    }
+
+    /**
+     * Binary Search:
+     * Note: Requires a sorted array in ascending order
+     *
+     * Big O: O(log N)
+     *
+     * @param numbers array of integers
+     * @param searchValue element to search for
+     * @return the index of the element if it exists, otherwise -1
+     */
+    public static int binarySearch(int[] numbers, int searchValue) {
+        int lowerBound = 0;
+        int upperBound = numbers.length - 1;
+
+        while (lowerBound <= upperBound) {
+            // Keeps dividing the array in half with each search
+            int middle = (upperBound + lowerBound) / 2;
+            int middleValue = numbers[middle];
+
+            if (middleValue == searchValue) {
+                return middle;
+            }
+            // Removes the right false (upper half) of the array since the search value is lower and has to be on the left half
+            else if (middleValue > searchValue) {
+                upperBound = middle - 1;
+            }
+            // Removes the left half (lower half) of the array since the search value is higher and has to be on the right half
+            else {
+                lowerBound = middle + 1;
             }
         }
         return -1; // -1 is returned if the search value doesn't exist in the array
--- DSAPractice/src/main/java/org/example/Main.java
+++ DSAPractice/src/main/java/org/example/Main.java
@@ -4,8 +4,8 @@ import org.example.Algorithms.SearchingAlgorithms;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {21, 10, 3, 6, 8, 40, 1, 12};
+        int[] numbers = {1, 2, 4, 5, 6, 10, 20, 24, 25, 30, 40, 45, 60};
 
-        System.out.println(SearchingAlgorithms.linearSearch(numbers, 6));
+        System.out.println(SearchingAlgorithms.binarySearch(numbers, 66));
     }
 }
